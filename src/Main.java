8cedf8fb63dc4956b4b3e4dd0f9ff3abbf176cc3
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-08-22 20:00:24 +0900

    Выполнил ДЗ №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1 - пишем код");
+        System.out.println("2 - делаем коммит и пуш");
+        System.out.println("3 - переходим в Github");
     }
 }
