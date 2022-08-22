c3650b093e2877531e60b8f749e9c01ef1628531
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-08-22 20:26:29 +0900

    Вторая часть ДЗ

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,10 @@ public class Main {
         float floatType = 13.928f;
         double doubleType = 9.54585126;
 
+        //2
+        float firstBoxer = 78.2f;
+        float secondBoxer = 82.7f;
+        System.out.println("Общий вес обоих боксёров: " + (firstBoxer + secondBoxer));
+        System.out.println("Разница в весе боксеров " + (firstBoxer < secondBoxer ? secondBoxer - firstBoxer : firstBoxer - secondBoxer) + " кг.");
     }
 }
